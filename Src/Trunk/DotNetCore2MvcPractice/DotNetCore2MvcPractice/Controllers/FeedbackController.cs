fd8387d95c82cd00d87a9be66a6a0796fcc6213a
Thomas Chen <thomaschen0224@gmail.com>
2019-09-02 10:16:37 -0500

    add feedback form

---
+++ Src/Trunk/DotNetCore2MvcPractice/DotNetCore2MvcPractice/Controllers/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using DotNetCore2MvcPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore2MvcPractice.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public FeedbackController()
+        {
+        }
+
+
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save to Database (Service or Repository)
+                return RedirectToAction("FeedbackComplete");
+            }
+            return View(feedback);
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
+
+    }
+}
